45582f5b6abe3e04811a2b05c54e19e94d13e634
skullyurd <47215203+skullyurd@users.noreply.github.com>
2022-02-06 12:03:50 +0100

    First state of multiplayer
    Map is made, 2 different clients can now connect with eachother.

---
+++ hatHolder_Multiplayer_Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
+using Photon.Realtime;
+
+public class Menu : MonoBehaviourPunCallbacks
+{
+    [Header("Screens")]
+    [SerializeField] private GameObject mainScreen;
+    [SerializeField] private GameObject lobbyScreen;
+
+    [Header("Main Screen")]
+    [SerializeField] private Button joinRoomButton;
+    [SerializeField] private Button createRoomButton;
+
+    [Header("Lobby Screen")]
+    [SerializeField] private TextMeshProUGUI playerListText;
+    [SerializeField] private Button startGameButton;
+
+    private void Start()
+    {
+        // these buttons aren't needed when you aren't connected yet
+        createRoomButton.interactable = false;
+        joinRoomButton.interactable = false;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        createRoomButton.interactable = true;
+        joinRoomButton.interactable = true;
+    }
+
+    void SetScreen(GameObject screen)
+    {
+        mainScreen.SetActive(false);
+        lobbyScreen.SetActive(false);
+
+        screen.SetActive(true);
+    }
+
+    public void OnCreateRoomButton(TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.CreateRoom(roomNameInput.text);
+    }
+
+    public void OnJoinRoomButton(TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.joinRoom(roomNameInput.text);
+    }
+
+    public void OnPlayerNameUpdate(TMP_InputField playerNameInput)
+    {
+        PhotonNetwork.NickName = playerNameInput.text;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SetScreen(lobbyScreen);
+
+        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdateLobbyUI();
+    }
+
+    [PunRPC]
+    public void UpdateLobbyUI()
+    {
+        playerListText.text = " ";
+
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            playerListText.text += player.NickName + "\n";
+        }
+
+        if (PhotonNetwork.IsMasterClient)
+            startGameButton.interactable = true;
+        else
+            startGameButton.interactable = false;
+    }
+
+    public void OnLeaveLobbyButton()
+    {
+        PhotonNetwork.LeaveRoom();
+        SetScreen(mainScreen);
+    }
+
+    public void OnStartGameButton()
+    {
+        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
+    }
+}
---
+++ hatHolder_Multiplayer_Game/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static NetworkManager instance;
+
+    // Setting the instance
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+            return;
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+    }
+
+    // connecting to master server
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //is making a attempt to make a room
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    //attemps to join a room
+    public void joinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("room created: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    [PunRPC]
+    //change scene using the system og photon
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+}
